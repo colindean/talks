4067a87d084feeb9a112c77519912fb0cf625492
Colin Dean <git@cad.cx>
2017-07-12 01:23:08 -0400

    Adds more exercises and sections to presentation

--- intro_to_plaintextaccounting/build_html.sh
+++ intro_to_plaintextaccounting/build_html.sh
@@ -16,6 +16,7 @@ for file in `ls *-$output`; do
   title=$(xmlstarlet sel -t -v '/_:html/_:head/_:title/text()' "$file" )
   content=$(xmlstarlet sel -t -c '/_:html/_:body/_:pre/node()' "$file" )
   echo -e "<h1>$title</h1><pre>$title\n$content</pre>" >> $output
+  rm $file
 done
 
 echo "</body></html>" >> $output
